e6da98d6349616c1f1640f13c61cb3cce26c56ce
Дмитрий Тебеньков <vitalya.tebenkov@gmail.com>
2026-06-26 09:31:34 +0500

    FormProducts изменение отображения таблицы

--- ShoesProject/FormProducts.Designer.cs
+++ ShoesProject/FormProducts.Designer.cs
@@ -28,11 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
             panel1 = new Panel();
-            dgvProducts = new DataGridView();
-            btnLogin = new Button();
             lblUserName = new Label();
+            btnLogut = new Button();
+            dgvProducts = new DataGridView();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dgvProducts).BeginInit();
             SuspendLayout();
@@ -40,52 +40,13 @@
             // panel1
             // 
             panel1.Controls.Add(lblUserName);
-            panel1.Controls.Add(btnLogin);
+            panel1.Controls.Add(btnLogut);
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(10, 10);
             panel1.Name = "panel1";
             panel1.Padding = new Padding(0, 0, 0, 10);
             panel1.Size = new Size(964, 40);
             panel1.TabIndex = 0;
-            // 
-            // dgvProducts
-            // 
-            dgvProducts.AllowUserToAddRows = false;
-            dgvProducts.AllowUserToDeleteRows = false;
-            dgvProducts.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
-            dgvProducts.BackgroundColor = Color.White;
-            dgvProducts.BorderStyle = BorderStyle.None;
-            dgvProducts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = SystemColors.Window;
-            dataGridViewCellStyle2.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
-            dgvProducts.DefaultCellStyle = dataGridViewCellStyle2;
-            dgvProducts.Dock = DockStyle.Fill;
-            dgvProducts.Location = new Point(10, 50);
-            dgvProducts.MultiSelect = false;
-            dgvProducts.Name = "dgvProducts";
-            dgvProducts.ReadOnly = true;
-            dgvProducts.RowHeadersVisible = false;
-            dgvProducts.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgvProducts.Size = new Size(964, 601);
-            dgvProducts.TabIndex = 1;
-            // 
-            // btnLogin
-            // 
-            btnLogin.BackColor = Color.MediumSpringGreen;
-            btnLogin.Dock = DockStyle.Right;
-            btnLogin.FlatAppearance.BorderSize = 0;
-            btnLogin.FlatStyle = FlatStyle.Flat;
-            btnLogin.Location = new Point(814, 0);
-            btnLogin.Name = "btnLogin";
-            btnLogin.Size = new Size(150, 30);
-            btnLogin.TabIndex = 5;
-            btnLogin.Text = "Войти";
-            btnLogin.UseVisualStyleBackColor = false;
             // 
             // lblUserName
             // 
@@ -98,6 +59,48 @@
             lblUserName.Text = "label1";
             lblUserName.TextAlign = ContentAlignment.MiddleRight;
             // 
+            // btnLogut
+            // 
+            btnLogut.BackColor = Color.MediumSpringGreen;
+            btnLogut.Dock = DockStyle.Right;
+            btnLogut.FlatAppearance.BorderSize = 0;
+            btnLogut.FlatStyle = FlatStyle.Flat;
+            btnLogut.Location = new Point(814, 0);
+            btnLogut.Name = "btnLogut";
+            btnLogut.Size = new Size(150, 30);
+            btnLogut.TabIndex = 5;
+            btnLogut.Text = "Выход";
+            btnLogut.UseVisualStyleBackColor = false;
+            btnLogut.Click += btnLogut_Click;
+            // 
+            // dgvProducts
+            // 
+            dgvProducts.AllowUserToAddRows = false;
+            dgvProducts.AllowUserToDeleteRows = false;
+            dgvProducts.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvProducts.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvProducts.BackgroundColor = Color.White;
+            dgvProducts.BorderStyle = BorderStyle.None;
+            dgvProducts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvProducts.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Window;
+            dataGridViewCellStyle1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            dataGridViewCellStyle1.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dgvProducts.DefaultCellStyle = dataGridViewCellStyle1;
+            dgvProducts.Dock = DockStyle.Fill;
+            dgvProducts.Location = new Point(10, 50);
+            dgvProducts.MultiSelect = false;
+            dgvProducts.Name = "dgvProducts";
+            dgvProducts.ReadOnly = true;
+            dgvProducts.RowHeadersVisible = false;
+            dgvProducts.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvProducts.Size = new Size(964, 601);
+            dgvProducts.TabIndex = 1;
+            // 
             // FormProducts
             // 
             AutoScaleDimensions = new SizeF(9F, 19F);
@@ -107,7 +110,7 @@
             Controls.Add(dgvProducts);
             Controls.Add(panel1);
             Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            Margin = new Padding(4, 4, 4, 4);
+            Margin = new Padding(4);
             Name = "FormProducts";
             Padding = new Padding(10);
             StartPosition = FormStartPosition.CenterScreen;
@@ -122,7 +125,7 @@
 
         private Panel panel1;
         private DataGridView dgvProducts;
-        private Button btnLogin;
+        private Button btnLogut;
         private Label lblUserName;
     }
 }
--- ShoesProject/FormProducts.cs
+++ ShoesProject/FormProducts.cs
@@ -1,18 +1,131 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
+﻿using Microsoft.EntityFrameworkCore;
+using ShoesProject.Models;
+using ShoesProject.Properties;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace ShoesProject
 {
     public partial class FormProducts : Form
     {
-        public FormProducts()
+
+        public User CurrentUser { get; private set; }
+        public bool IsGuest { get; private set; }
+
+        public FormProducts(User user, bool guest)
         {
             InitializeComponent();
+
+            var colPhoto = new DataGridViewImageColumn();
+            colPhoto.Name = "colPhoto";
+            colPhoto.ImageLayout = DataGridViewImageCellLayout.Zoom;
+            colPhoto.Width = 200;
+            colPhoto.FillWeight = 30;
+
+            var colInfo = new DataGridViewTextBoxColumn();
+            colInfo.Name = "colInfo";
+            colInfo.FillWeight = 60;
+            colInfo.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+
+            var colDiscount = new DataGridViewTextBoxColumn();
+            colDiscount.Name = "colDiscount";
+            colDiscount.FillWeight = 10;
+            colDiscount.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            dgvProducts.Columns.AddRange([
+                colPhoto, colInfo, colDiscount
+            ]);
+
+            CurrentUser = user;
+            IsGuest = guest;
+
+            lblUserName.Text = IsGuest ? "Гость" : CurrentUser.FullName;
+
+            LoadProducts();
+        }
+        private void LoadProducts()
+        {
+            try
+            {
+                using (var db = new ShopDbContext())
+                {
+                    var products = db.Products
+                        .Include(i => i.Category)
+                        .Include(i => i.Manufacturer)
+                        .Include(i => i.Supplier)
+                        .Include(i => i.Measure)
+                        .Include(i => i.ProductType)
+                        .ToList();
+
+                    dgvProducts.SuspendLayout();
+                    dgvProducts.Rows.Clear();
+
+                    foreach (var product in products)
+                    {
+                        int rowIndex = dgvProducts.Rows.Add();
+                        var row = dgvProducts.Rows[rowIndex];
+
+                        row.Cells["colPhoto"].Value = LoadProductImage(product.PhotoUrl);
+                        row.Cells["colInfo"].Value = FormatProductInfo(product);
+
+                        row.Cells["colDiscount"].Value = $"{product.Discount}%";
+                        row.Cells["colDiscount"].Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                    }
+
+                    dgvProducts.ResumeLayout();
+                    dgvProducts.AutoResizeRows(DataGridViewAutoSizeRowsMode.AllCells);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private string FormatProductInfo(Product product)
+        {
+            string priceText;
+
+            if (product.Discount > 0)
+            {
+                decimal finalPrice = product.Price * (100 - product.Discount) / 100;
+                priceText = $"Цена: {product.Price:C} -> {finalPrice:C}";
+            }
+            else
+            {
+                priceText = $"Цена: {product.Price:C}";
+            }
+
+            return $"{product.Category.CategoryName} | {product.ProductType.ProdType} " + Environment.NewLine +
+                 $"Описание товара: {product.Description}" + Environment.NewLine +
+                 $"Производитель: {product.Manufacturer.ManufacturerName}" + Environment.NewLine +
+                 $"Поставщик: {product.Supplier.SupplierName}" + Environment.NewLine +
+                 $"{priceText}" + Environment.NewLine +
+                 $"Еденица измерения: {product.Measure.MeasureName}" + Environment.NewLine +
+                 $"Количество на складе: {product.CointInStock}" + Environment.NewLine;
+        }
+        private Image? LoadProductImage(string photoUrl)
+        {
+            if (!string.IsNullOrEmpty(photoUrl) && System.IO.File.Exists(photoUrl))
+            {
+                return Image.FromFile(photoUrl);
+            }
+
+            Bitmap bmp = new Bitmap(150, 100);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+                g.DrawRectangle(Pens.LightGray, 0, 0, 149, 99);
+            }
+
+            return Resources.picture;
+        }
+
+        private void btnLogut_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
         }
     }
 }
